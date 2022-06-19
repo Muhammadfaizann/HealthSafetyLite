c80a8a35a82f8b6ceac0a86b722e52f860568357
Burhan <burhan.ahmed@estratcom.com>
2022-06-19 22:04:06 +0500

    Navigation functionality to menu buttons and dynmic assessment page design

--- HealthSafetyAppLite/Views/LandingPageDetail.xaml.cs
+++ HealthSafetyAppLite/Views/LandingPageDetail.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using HealthSafetyAppLite.ViewModels;
+using HealthSafetyAppLite.Views.TopicViews;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +21,22 @@ namespace HealthSafetyAppLite.Views
             InitializeComponent();
             this.BindingContext = new LandingPageViewModel(Navigation);
         }
-        
-        
+
+       
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped_2(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+           Navigation.PushAsync(new  DynamicToolPage());
+        }
     }
 }
