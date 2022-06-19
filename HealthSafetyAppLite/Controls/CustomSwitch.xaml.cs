be56109893c1872a92fe1c5b9636a9d9e5e22afd
Muahmmad Faizan <i130205@nu.edu.pk>
2022-06-19 10:51:06 -0700

    custom switch

---
+++ HealthSafetyAppLite/Controls/CustomSwitch.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HealthSafetyAppLite.Controls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class CustomSwitch : Frame
+    {
+        public CustomSwitch()
+        {
+            InitializeComponent();
+        }
+    }
+}
